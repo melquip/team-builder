085f1674f5e422194c906a79a104095c56548682
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-18 12:42:27 +0100

    Completed Steps 1 and 2

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
 
+const initialTeam = [
+	{
+		name: 'Melquisedeque',
+		email: 'melqui@gmail.com',
+		role: 'User Interface Developer',
+	},
+	{
+		name: 'Oladimegi',
+		email: 'oladimegi@gmail.com',
+		role: 'Front-end Engineer',
+	},
+	{
+		name: 'Austin',
+		email: 'austin@gmail.com',
+		role: 'Back-end Developer',
+	}
+];
+const initialMemberForm = {
+	name: '',
+	email: '',
+	role: '',
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [team, setTeam] = useState(initialTeam);
+	const [memberForm, setMemberForm] = useState(initialMemberForm)
+	const handleInputChange = e => {
+		setMemberForm(
+			{
+				...memberForm,
+				[e.target.id]: e.target.value,
+			}
+		)
+	}
+	const handleMemberFormSubmit = e => {
+		e.preventDefault();
+		setTeam(
+			[
+				...team,
+				{ ...memberForm }
+			]
+		);
+		setMemberForm(initialMemberForm);
+	}
+	return (
+		<div className="App">
+			<div className="team">
+				{
+					team.map(member => (
+						<div key={member.email} className="member">
+							{member.name}<br />
+							{member.email}<br />
+							{member.role}<br />
+						</div>
+					))
+				}
+			</div>
+			<Form
+				data={memberForm}
+				handleChange={handleInputChange}
+				handleSubmit={handleMemberFormSubmit}
+			/>
+		</div>
+	);
 }
 
 export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Form({ data, handleChange, handleSubmit }) {
+	const { name, email, role } = data;
+	return (
+		<>
+			<h1>Add Team Member</h1>
+			<form>
+				<label htmlFor="name">Name:</label>
+				<input value={name} onChange={handleChange} id="name" name="name" type="text" required />
+				<br />
+				<label htmlFor="email">Email:</label>
+				<input value={email} onChange={handleChange} id="email" name="email" type="email" required />
+				<br />
+				<label htmlFor="role">Role:</label>
+				<input value={role} onChange={handleChange} id="role" name="role" type="text" required />
+				<br />
+				<button onClick={handleSubmit}>Submit</button>
+			</form>
+		</>
+	)
+}
